b921e6991370297080d557326e59f8046b5a8ab0
Peter Jekel <peter@thejekels.com>
2013-04-18 09:16:32 -0400

    Added new test modules

--- tests/module.js
+++ tests/module.js
@@ -1,5 +1,10 @@
 define(["doh/main", "require"], function(doh, require){
 
+	// Make sure the keys functionality is tested first as it is the foundation
+	// of most other functions.
+	
+	doh.register("Store Keys", require.toUrl("./test_Keys.html"), 999999);
+
 	doh.register("Natural Store", require.toUrl("./test_NaturalStore.html"), 999999);
 	doh.register("Indexed Store", require.toUrl("./test_IndexedStore.html"), 999999);
 	doh.register("Compound Keys", require.toUrl("./test_Compound_Keys.html"), 999999);
@@ -9,5 +14,6 @@ define(["doh/main", "require"], function(doh, require){
 	doh.register("Store Indexes", require.toUrl("./test_Indexes.html"), 999999);
 	doh.register("Query Engine", require.toUrl("./test_Query_Engine.html"), 999999);
 	doh.register("Hierarchy Extension", require.toUrl("./test_Hierarchy_Extension.html"), 999999);
+	doh.register("Ancestry Extension", require.toUrl("./test_Ancestry_Extension.html"), 999999);
 
 });

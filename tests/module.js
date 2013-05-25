133c7207b45697167bf96b7336075d9d4607302c
Peter Jekel <peter@thejekels.com>
2013-05-25 14:14:24 -0400

    Updated and new unit test

--- tests/module.js
+++ tests/module.js
@@ -1,9 +1,10 @@
 define(["doh/main", "require"], function(doh, require){
 
-	// Make sure the keys functionality is tested first as it is the foundation
-	// of most other functions.
+	// Make sure the keys and listener functionality is tested first as it is the
+	// foundation of most other functions.
 	
 	doh.register("Store Keys", require.toUrl("./test_Keys.html"), 999999);
+	doh.register("Listeners", require.toUrl("./test_Listeners.html"), 999999);
 
 	doh.register("Natural Store", require.toUrl("./test_NaturalStore.html"), 999999);
 	doh.register("Indexed Store", require.toUrl("./test_IndexedStore.html"), 999999);
@@ -15,9 +16,10 @@ define(["doh/main", "require"], function(doh, require){
 	doh.register("Index Cursors", require.toUrl("./test_Index_Cursors.html"), 999999);
 	doh.register("Store Ranges", require.toUrl("./test_Ranges.html"), 999999);
 	doh.register("Query Engine", require.toUrl("./test_Query_Engine.html"), 999999);
+	doh.register("CORS Extension", require.toUrl("./test_CORS_Extension.html"), 999999);
 	doh.register("Hierarchy Extension", require.toUrl("./test_Hierarchy_Extension.html"), 999999);
 	doh.register("Ancestry Extension", require.toUrl("./test_Ancestry_Extension.html"), 999999);
 	doh.register("Watch Extension", require.toUrl("./test_Watch_Extension.html"), 999999);
-	doh.register("CORS Extension", require.toUrl("./test_CORS_Extension.html"), 999999);
+	doh.register("Observable Extension", require.toUrl("./test_Observable_Extension.html"), 999999);
 
 });

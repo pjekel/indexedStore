9aa42dd0d670c1a698a25cb2095bf1c3ab1fe8f1
Peter Jekel <peter@thejekels.com>
2013-05-31 18:12:05 -0400

    Preserve dummy listener

--- _base/Eventer.js
+++ _base/Eventer.js
@@ -6,19 +6,29 @@ define(["dojo/_base/declare",
 			 ], function (declare, Lib, Event, EventTarget, createError) {
 	"use strict";
 	
+	// module
+	//		indexedStore/_base/Eventer
+	
 	var StoreError = createError("Eventer");		// Create the StoreError type.
 	var isObject   = Lib.isObject;
 	var isString   = Lib.isString;
 	
 	function removeListeners(source, type) {
 		// summary:
+		//		Remove all event listeners for the given type with the exception of
+		//		the dummy listener. Preserving the dummy listener guarantees advice
+		//		can still be added if required.
 		// source: EventTarget
 		// type: type
-		var listeners = source.getEventListeners(type);
-		if (listeners) {
-			listeners.forEach( function (listener) {
-				source.removeEventListener( type, listener );
-			});
+		//		Event type.
+		// tag:
+		//		Private
+		var i, listeners = source.getEventListeners(type);
+		if (listeners && listeners.length > 1) {
+			// Keep the dummy listener so advice can still be added....
+			for (i=1; i<listeners.length; i++) {
+				source.removeEventListener( type, listeners[i] );
+			}
 		}
 	}
 	
@@ -77,28 +87,27 @@ define(["dojo/_base/declare",
 			// custom: Boolean?
 			// tag:
 			//		Public
+			var props = properties;
 			if (type && isString(type)) {
-				if (!source.suppressEvents) {
-					if (custom) {
-						var custProp = properties;
-						if (!properties.detail) {
-							custProp = {
-								cancelable: !!properties.cancelable,
-								bubbles: !!properties.bubbles,
-								detail: properties
-							};
-							delete  properties.cancelable;
-							delete  properties.bubbles;
-						}
-						properties = custProp;
+				if (custom && props) {
+					var custProp = props;
+					if (!props.detail) {
+						custProp = {
+							cancelable: !!props.cancelable,
+							bubbles: !!props.bubbles,
+							detail: props
+						};
+						delete  props.cancelable;
+						delete  props.bubbles;
 					}
-					var event = new Event (type, properties);
-					source.dispatchEvent( event );
+					props = custProp;
 				}
+				var event = new Event (type, props);
+				source.dispatchEvent( event );
 			} else {
 				throw new StoreError( "TypeError", "emit", "invalid type property");
 			}
-		}
+		};
 
 		if (source instanceof EventTarget) {
 			if (types) {
